98e234190e99729448a95f445fb19b22ddaf60da
fidel <fidel@yafp.de>
2019-05-23 19:10:48 +0200

    20190523 - Removing no-longer used files

--- renderer.js
+++
@@ -1,8 +0,0 @@
-// This file is required by the index.html file and will
-// be executed in the renderer process for that window.
-// All of the Node.js APIs are available in this process.
-
-console.log("renderer.js ::: Start");
-
-
-console.log("renderer.js ::: End");
