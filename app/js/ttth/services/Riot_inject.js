68d3b87f7852013f89117ddc12e048bf0790cbba
fidel <fidel@yafp.de>
2019-08-28 10:29:07 +0200

    20190828 - Adding some post work for #101 - riot.im integration. Untested unread message function

---
+++ app/js/ttth/services/Riot_inject.js
@@ -0,0 +1,30 @@
+const {ipcRenderer} = require("electron");
+
+
+/**
+* @name serviceRiotGetUnreadMessageCount
+* @summary Gets the amount of unread messages of the service Riot.im
+* @description Gets the amount of unread messages of the service Riot.im
+*/
+function serviceRiotGetUnreadMessageCount()
+{
+    console.log("serviceRiotGetUnreadMessageCount ::: Start");
+
+    var a=document.getElementsByClassName("mx_RoomTile_nameContainer"),b=0;
+    for(i=0;i<a.length;i++)
+    {
+        var c=a[i].getElementsByClassName("mx_RoomTile_badge");
+        for(ii=0;ii<c.length;ii++)parseInt(c[ii].textContent.trim())%1===0&&(b+=parseInt(c[ii].textContent.trim()))
+    }
+
+    console.log("serviceRiotGetUnreadMessageCount ::: Unread messages in Riot: _" + b + "_.");
+
+    ipcRenderer.sendToHost(b);
+
+    console.log("serviceRiotGetUnreadMessageCount ::: End");
+}
+
+ipcRenderer.on("request", function()
+{
+    ipcRenderer.sendToHost(serviceRiotGetUnreadMessageCount());
+});
