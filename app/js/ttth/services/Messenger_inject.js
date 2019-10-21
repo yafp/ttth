2cfbc9efaacc93f508156547d64b2605f2982c7c
fidel <fidel@yafp.de>
2019-10-21 19:14:56 +0200

    20191021 - Adding unread message count function for facebook messenger - see #132

---
+++ app/js/ttth/services/Messenger_inject.js
@@ -0,0 +1,30 @@
+const {ipcRenderer} = require("electron");
+
+
+/**
+* @name serviceMessengerGetUnreadMessageCount
+* @summary Gets the amount of unread messages of the service Messenger
+* @description Gets the amount of unread messages of the service Messenger
+*/
+function serviceMessengerGetUnreadMessageCount()
+{
+    console.log("serviceMessengerGetUnreadMessageCount ::: Checking unread message count");
+
+    let count = document.querySelectorAll('._5fx8:not(._569x),._1ht3:not(._569x)').length;
+    const messageRequestsElement = document.querySelector('._5nxf');
+    if (messageRequestsElement) 
+    {
+        count += parseInt(messageRequestsElement.innerHTML, 10);
+    }
+
+    console.log("serviceMessengerGetUnreadMessageCount ::: Total Messenger chats with unread messages: " + count);
+
+    // send back from webview to main
+    ipcRenderer.sendToHost(counter);
+}
+
+
+ipcRenderer.on("request", function()
+{
+    ipcRenderer.sendToHost(serviceMessengerGetUnreadMessageCount());
+});
