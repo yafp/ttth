eebf69605fafbb86aec63412499d84b83c3c594d
fidel <fidel@yafp.de>
2019-10-22 20:06:36 +0200

    20191022 - SAdding support for SteamChat - see #135

---
+++ app/js/ttth/services/SteamChat_inject.js
@@ -0,0 +1,24 @@
+const {ipcRenderer} = require("electron");
+
+
+/**
+* @name serviceSteamChatGetUnreadMessageCount
+* @summary Gets the amount of unread messages of the service ICQ
+* @description Gets the amount of unread messages of the service ICQ
+*/
+function serviceSteamChatGetUnreadMessageCount()
+{
+    console.log("serviceSteamChatGetUnreadMessageCount ::: Checking unread message count");
+
+    var e=document.getElementsByClassName("unread_message_count_value"),t=0;for(i=0;i<e.length;i++)t+=isNaN(parseInt(e[i].innerHTML.trim())) || e[i].parentNode.style.display === "none" ? 0 : parseInt(e[i].innerHTML.trim());
+
+    console.log("serviceSteamChatGetUnreadMessageCount ::: Total Steam unread messages: " + e);
+
+    ipcRenderer.sendToHost(e);
+}
+
+
+ipcRenderer.on("request", function()
+{
+    ipcRenderer.sendToHost(serviceSteamChatGetUnreadMessageCount());
+});
