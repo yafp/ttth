bcc9fd9364cfdb47099c31bb873d73af80a2ef2d
fidel <fidel@yafp.de>
2019-10-24 19:28:07 +0200

    20191024 - Adding support for Gitter - see #140

---
+++ app/js/ttth/services/Gitter_inject.js
@@ -0,0 +1,26 @@
+const {ipcRenderer} = require("electron");
+
+
+/**
+* @name serviceGitterGetUnreadMessageCount
+* @summary Gets the amount of unread messages of the service Gitter
+* @description Gets the amount of unread messages of the service Gitter
+*/
+function serviceGitterGetUnreadMessageCount()
+{
+    console.log("serviceGitterGetUnreadMessageCount ::: Checking unread message count");
+
+    var t;
+    var e=document.getElementsByClassName("room-item__unread-indicator"),t=0;for(i=0;i<e.length;i++)t+=isNaN(parseInt(e[i].innerHTML.trim())) ? 0 : parseInt(e[i].innerHTML.trim());
+
+    console.log("serviceGitterGetUnreadMessageCount ::: Total GoogleMail unread messages: " + t);
+
+    // send back from webview to main
+    ipcRenderer.sendToHost(t);
+}
+
+
+ipcRenderer.on("request", function()
+{
+    ipcRenderer.sendToHost(serviceGitterGetUnreadMessageCount());
+});
