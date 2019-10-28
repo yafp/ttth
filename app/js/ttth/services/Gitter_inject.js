46b1466206d6738f1c23b66aa55cf284523a7262
fidel <fidel@yafp.de>
2019-10-28 19:33:37 +0100

    20191028 - Improving gitter inject code - see #140

--- app/js/ttth/services/Gitter_inject.js
+++ app/js/ttth/services/Gitter_inject.js
@@ -11,7 +11,12 @@ function serviceGitterGetUnreadMessageCount()
     console.log("serviceGitterGetUnreadMessageCount ::: Checking unread message count");
 
     var t;
-    var e=document.getElementsByClassName("room-item__unread-indicator"),t=0;for(i=0;i<e.length;i++)t+=isNaN(parseInt(e[i].innerHTML.trim())) ? 0 : parseInt(e[i].innerHTML.trim());
+    var i;
+    var e=document.getElementsByClassName("room-item__unread-indicator"),t=0;
+    for(i=0;i<e.length;i++)
+    {
+        t+=isNaN(parseInt(e[i].innerHTML.trim())) ? 0 : parseInt(e[i].innerHTML.trim());
+    }
 
     console.log("serviceGitterGetUnreadMessageCount ::: Total GoogleMail unread messages: " + t);
 
